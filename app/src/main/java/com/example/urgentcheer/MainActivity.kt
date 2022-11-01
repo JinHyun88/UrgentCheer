4a16f486251a3ee4faf87374b11b4a04e55eb118
Laptop <Laptop@KJH>
2022-11-01 16:11:13 +0900

    add : Kakao Map Api

--- app/src/main/java/com/example/urgentcheer/MainActivity.kt
+++ app/src/main/java/com/example/urgentcheer/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.urgentcheer
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import net.daum.mf.map.api.MapView
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapView = MapView(this)
+        val kakaoMapView = findViewById<ConstraintLayout>(R.id.kakaoMapView)
+        kakaoMapView.addView(mapView)
     }
 }
