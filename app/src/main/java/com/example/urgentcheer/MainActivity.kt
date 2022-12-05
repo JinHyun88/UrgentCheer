9f439f7c9f9c170e90295bdb0bcf8d0ace09a9fa
KJH <qwekimjh@gmail.com>
2022-12-05 10:28:01 +0900

    Add Jsoup Test

--- app/src/main/java/com/example/urgentcheer/MainActivity.kt
+++ app/src/main/java/com/example/urgentcheer/MainActivity.kt
@@ -1,24 +1,54 @@
 package com.example.urgentcheer
 
-import android.content.pm.PackageManager
-import android.os.Build
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.constraintlayout.widget.ConstraintLayout
-import net.daum.mf.map.api.MapView
-import java.security.MessageDigest
+import com.example.urgentcheer.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import org.json.JSONException
+import org.json.JSONObject
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.P)
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    private val searchUrl = "https://api.hangang.msub.kr"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val mapView = MapView(this)
-        val kakaoMapView = findViewById<ConstraintLayout>(R.id.kakaoMapView)
-        kakaoMapView.addView(mapView)
+
+        val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
+            throwable.printStackTrace()
+        }
+
+        CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch {
+            val doc = Jsoup.connect(searchUrl).get().getElementsByTag("pre")[0].text()
+            delay(1000)
+            Log.d(TAG, "onCreate: $doc")
+
+//            try{
+//                val status = JSONObject(doc).getString("status")
+//                val temp = JSONObject(doc).getString("temp")
+//                val time = JSONObject(doc).getString("time")
+//
+//                Log.d(TAG, "onCreate: $status $temp $time")
+//            } catch (e:JSONException){
+//                e.printStackTrace()
+//            }
+        }
+
     }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
+    }
+
 }
